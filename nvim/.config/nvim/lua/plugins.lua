75b1f3b266b130c0929928b7a3fc1483c24e0f0a
David Barnett <davidb@rnett.nz>
2024-08-15 20:24:48 +1000

    Add neogit

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -299,6 +299,7 @@ require("lazy").setup({
       end
   },
   {
+    -- nvim-treesitter {{{
     'nvim-treesitter/nvim-treesitter',
     main = 'nvim-treesitter.configs',
     build = ':TSUpdate', 
@@ -317,8 +318,35 @@ require("lazy").setup({
       auto_install = true,
       highlight = {
         enable = true,
-      }
+      },
+      incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+        },
+      },
+      textobjects = {
+        move = {
+        enable = true,
+        goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+        goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+        goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+        goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+      },
+  },
     }
+    --- }}}
+  },
+  {
+    -- nvim-treesitter-textobjects
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+
   },
   {
     -- lualine {{{
@@ -410,6 +438,17 @@ require("lazy").setup({
       }
       -- }}}
   },
+  -- gitsigns {{{
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = true
+  },
+  -- }}}
 })
 
 
