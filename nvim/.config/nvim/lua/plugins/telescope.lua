f97f990653cb55ae3c9a42c0e231d78f8ff50c54
David Barnett <davidb@rnett.nz>
2025-06-05 08:00:28 +1000

    tele desk and changes

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -18,25 +18,23 @@ return {
         config = function()
             local builtin = require("telescope.builtin")
             -- Add leader shortcuts
-            vim.keymap.set("n", "<leader><space>", builtin.resume, {})
-            vim.keymap.set("n", "<leader>f-", builtin.find_files, {})
-            vim.keymap.set("n", "<leader>ff", builtin.git_files, {})
-            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-            vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+            vim.keymap.set("n", "<leader><space>", builtin.resume, { desc = "Resume telescope picker" })
+            vim.keymap.set("n", "<leader>f-", builtin.find_files, { desc = "Find files" })
+            vim.keymap.set("n", "<leader>ff", builtin.git_files, { desc = "Find files (git)" })
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Select buffers" })
             vim.keymap.set("n", "zf", builtin.spell_suggest, {})
-            vim.keymap.set("n", "<leader>fs", builtin.treesitter, {})
 
             -- Helps
-            vim.keymap.set("n", "<leader>fq", builtin.quickfix, {})
-            vim.keymap.set("n", "<leader>fm", builtin.keymaps, {})
-            vim.keymap.set("n", "<leader>fp", builtin.pickers, {})
+            vim.keymap.set("n", "<leader>fq", builtin.quickfix, { desc = "Open quickfix picker" })
+            vim.keymap.set("n", "<leader>fm", builtin.keymaps, { desc = "Keymaps help" })
+            vim.keymap.set("n", "<leader>fp", builtin.pickers, { desc = "Open previous picker menu" })
 
             -- LSP
-            vim.keymap.set("n", "<leader>lr", builtin.lsp_references, {})
-            vim.keymap.set("n", "<leader>li", builtin.lsp_incoming_calls, {})
-            vim.keymap.set("n", "<leader>lo", builtin.lsp_outgoing_calls, {})
-            vim.keymap.set("n", "<leader>ls", builtin.lsp_document_symbols, {})
+            vim.keymap.set("n", "glr", builtin.lsp_references, { desc = "Search LSP references" })
+            vim.keymap.set("n", "gli", builtin.lsp_incoming_calls, { desc = "Search LSP incoming calls" })
+            vim.keymap.set("n", "glo", builtin.lsp_outgoing_calls, { dessc = "Search out-going calls" })
+            vim.keymap.set("n", "<leader>ls", builtin.lsp_document_symbols, { desc = "List document dymbols from LSP" })
         end
         -- }}}
     },
